2f932c83e8d40a00d0faad378c89797aee43c5fb
Shadow <djshadow@live.com.au>
2021-06-19 11:08:06 +1000

    Project Radical Start

---
+++ app/src/main/java/com/shadow/androidguipartb/MainActivity.java
@@ -0,0 +1,19 @@
+package com.shadow.androidguipartb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+}
